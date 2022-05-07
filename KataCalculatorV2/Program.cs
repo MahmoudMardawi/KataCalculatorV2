1b4795e17bc1ca7de60f4474db65e337f4b09df7
MahmoudMardawi <46374856+MahmoudMardawi@users.noreply.github.com>
2022-05-07 18:33:15 +0300

    Initial Commit

---
+++ KataCalculatorV2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KataCalculatorV2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
