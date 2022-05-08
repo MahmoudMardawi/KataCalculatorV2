41715d775309386bcea28f996477cf0b3605862d
Mahmoud Mardawi <Mahmoud.Mardawi@foothillsolutions.com>
2022-05-08 12:35:29 +0300

    Create  Class to enter the data (Data Input)

--- KataCalculatorV2/Program.cs
+++ KataCalculatorV2/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using KataCalculatorV2.Modules;
+using KataCalculatorV2.Services;
+using System;
 
 namespace KataCalculatorV2
 {
@@ -6,7 +8,13 @@ namespace KataCalculatorV2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CalculatorModules calculatorModules = new CalculatorModules();
+            Tax tax = new Tax();
+            PrintInfo printInfo = new PrintInfo(tax);
+            DataInputUtilities.dataInputUtilities(calculatorModules);
+
+            printInfo.Print(calculatorModules);
+            
         }
     }
 }
