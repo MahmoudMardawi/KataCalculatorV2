a46413df05a00e01e21a0de745ff05289d8fbbfc
Mahmoud Mardawi <Mahmoud.Mardawi@foothillsolutions.com>
2022-05-08 12:43:01 +0300

    Main class for test

--- KataCalculatorV2/Program.cs
+++ KataCalculatorV2/Program.cs
@@ -1,12 +1,18 @@
-﻿using System;
-
+﻿using KataCalculatorV2.Modules;
+using KataCalculatorV2.Services;
+using System;
 namespace KataCalculatorV2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CalculatorModules calculatorModules = new CalculatorModules();
+            Tax tax = new Tax();
+            PrintInfo printInfo = new PrintInfo(tax);
+            DataInputUtilities.dataInputUtilities(calculatorModules);
+
+            printInfo.Print(calculatorModules);
         }
     }
 }
