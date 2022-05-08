d975d821fa09d7b2527e841f9a918741039a218d
Mahmoud Mardawi <Mahmoud.Mardawi@foothillsolutions.com>
2022-05-08 14:12:45 +0300

    Create interface for discount methods

---
+++ KataCalculatorV2/Interfaces/IDiscount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataCalculatorV2.Interfaces
+{
+    internal interface IDiscount
+    {
+        public double discountValue(CalculatorModels calculator);
+        public double discountAmount(CalculatorModels calculator);
+        public double priceAfterdiscount(CalculatorModels calculator);
+    }
+}
