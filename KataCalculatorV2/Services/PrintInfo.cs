6a9b4dc6d8214ab0e52d49c3c10f623f981dd85f
Mahmoud Mardawi <Mahmoud.Mardawi@foothillsolutions.com>
2022-05-08 12:33:39 +0300

    Create Print Class to print the output

--- KataCalculatorV2/Services/PrintInfo.cs
+++ KataCalculatorV2/Services/PrintInfo.cs
@@ -15,10 +15,13 @@ namespace KataCalculatorV2.Services
             this.tax = tax;
         }
 
-        public  void Print(CalculatorModules calculator)
+        public void Print(CalculatorModules calculator)
         {
-            
-            Console.WriteLine(tax.priceAfterTax(calculator).ToString());
+            PrintInfo printInfo = new PrintInfo(tax);
+
+            Console.WriteLine($"Book name = {calculator.ProductName} , UPC = {calculator.UPC} , Price = {calculator.price.ToString("C2")}\n"+
+                $", Price before tax {calculator.price.ToString("C2")} and after {calculator.taxValue} % tax ( {printInfo.tax.TaxAmount(calculator).ToString("C2")} ) .\n" +
+                $"And Price after tax = {printInfo.tax.priceAfterTax(calculator).ToString("C2")}");
         }
     }
 }
