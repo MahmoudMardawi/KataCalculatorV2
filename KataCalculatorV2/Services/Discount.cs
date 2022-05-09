c2c6335042ca13b06d9018814ce508d90e3af310
Mahmoud Mardawi <Mahmoud.Mardawi@foothillsolutions.com>
2022-05-09 03:19:11 +0300

    Implement IDiscount methods

---
+++ KataCalculatorV2/Services/Discount.cs
@@ -0,0 +1,15 @@
+﻿using KataCalculatorV2.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataCalculatorV2.Services
+{
+    internal class Discount : IDiscount
+    {
+        public double discountAmount(CalculatorModels calculator) => calculator.price * calculator.discountValue / 100; 
+        public double priceAfterdiscount(CalculatorModels calculator) => discountAmount(calculator)-calculator.price;
+    }
+}
