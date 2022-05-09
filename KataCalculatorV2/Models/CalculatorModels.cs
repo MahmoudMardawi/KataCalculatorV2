37c5508718e693c4d915ef935e6cb0725f2b8824
Mahmoud Mardawi <Mahmoud.Mardawi@foothillsolutions.com>
2022-05-09 03:18:01 +0300

    Add discountValue properity

--- KataCalculatorV2/Models/CalculatorModels.cs
+++ KataCalculatorV2/Models/CalculatorModels.cs
@@ -12,5 +12,6 @@ namespace KataCalculatorV2
         public string ProductName { get; set; }
         public int UPC { set; get; }
         public double price { set; get; }
+        public double discountValue { get; set; }
     }
 }
