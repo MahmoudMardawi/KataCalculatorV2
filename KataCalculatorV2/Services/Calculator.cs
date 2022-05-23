6af1ab4901197d350de7600531a8a51c5b125bf1
Mahmoud Mardawi <Mahmoud.Mardawi@foothillsolutions.com>
2022-05-23 17:59:08 +0300

    Create class Calcuator to make the necessary operations

---
+++ KataCalculatorV2/Services/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using KataCalculatorV2.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataCalculatorV2.Services
+{
+    internal class Calculator
+    {
+        ITax tax;
+        IDiscount discount;
+        public Calculator(ITax tax, IDiscount discount)
+        {
+            this.tax = tax;
+            this.discount = discount;
+        }
+        public decimal PriceAfterTax(CalculatorModel calculatorModel)
+        {
+            decimal result = tax.priceAfterTax(calculatorModel);
+            return result;
+        }
+        public decimal TaxAmount(CalculatorModel calculatorModel)
+        {
+            decimal result = tax.TaxAmount(calculatorModel);
+            return result;
+        } 
+        public decimal 
+       
+    }
+}
