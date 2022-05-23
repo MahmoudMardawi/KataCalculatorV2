9916d4908b8756aa24bd9bc035a0212614be49cc
MahmoudMardawi <46374856+MahmoudMardawi@users.noreply.github.com>
2022-05-24 00:05:40 +0300

    Add ProductInfo class to implement IProductInfo methods

---
+++ KataCalculatorV2/Services/ProductInfo.cs
@@ -0,0 +1,23 @@
+﻿using KataCalculatorV2.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataCalculatorV2.Services
+{
+    internal class ProductInfo : IProductInfo
+    {
+        CalculatorModel calculatorModel;
+        public ProductInfo(CalculatorModel calculatorModel)
+        {
+            this.calculatorModel = calculatorModel;
+        }
+        public string ProductName() => calculatorModel.ProductName;
+        public decimal Price() => calculatorModel.Price;
+        public int UPC() => calculatorModel.UPC;
+        public decimal TaxValue()=> calculatorModel.TaxValue;
+        public decimal DiscountValue() => calculatorModel.DiscountValue;
+    }
+}
