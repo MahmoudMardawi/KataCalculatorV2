a25c2d23698d3e426065239baf3990b79c226c49
MahmoudMardawi <46374856+MahmoudMardawi@users.noreply.github.com>
2022-05-24 00:05:03 +0300

    Add IProductInfo interface to retrieve product data

---
+++ KataCalculatorV2/Interfaces/IProductInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataCalculatorV2.Interfaces
+{
+    internal interface IProductInfo
+    {
+        public string ProductName();
+        public decimal Price();
+        public int UPC();
+        public decimal TaxValue();
+        public decimal DiscountValue();
+    }
+}
