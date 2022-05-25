5c2ccc02d2c37fe40880029a08f7cb79c3571ada
Mahmoud Mardawi <Mahmoud.Mardawi@foothillsolutions.com>
2022-05-25 12:24:37 +0300

    Change access modifier to public for testing

---
+++ KataCalculatorV2/Interfaces/ICalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataCalculatorV2.Interfaces
+{
+    public interface ICalculator
+    {
+  
+        public decimal PriceAfterTax();
+        public decimal TaxAmount();
+        public decimal PriceAfterDiscount();
+        public decimal DiscountAmount();
+        public decimal TotalPrice();
+    }
+}
--- KataCalculatorV2/Interfaces/IProductInfo.cs
+++ KataCalculatorV2/Interfaces/IProductInfo.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace KataCalculatorV2.Interfaces
 {
-    internal interface IProductInfo
+    public interface IProductInfo
     {
         public string ProductName();
         public decimal Price();
