05bb95430b7cf89d67b4ee74c4aa0bd92eda60d9
Mahmoud Mardawi <Mahmoud.Mardawi@foothillsolutions.com>
2022-05-25 12:26:33 +0300

    Change access modifier to public to support testing class

--- KataCalculatorV2/Models/CalculatorModel.cs
+++ KataCalculatorV2/Models/CalculatorModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace KataCalculatorV2
 {
-    internal class CalculatorModel
+    public class CalculatorModel
     {
         public decimal TaxValue { get; set; }
         public string ProductName { get; set; }
